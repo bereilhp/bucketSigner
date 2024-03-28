14762569504c8f84421edd58417c79878d220513
bereilhp <bereilhp@gmail.com>
2024-03-28 16:39:09 +0100

    working microservice

---
+++ connect.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const uri = process.env.IST_SHARED_MONGODB; 
+
+const client = new MongoClient(uri);
+
+const dbName = "dotLocalStore";
+
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB Atlas");
+    return client.db(dbName);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+}
+
+export function closeDatabase() {
+  if (client.isConnected()) {
+    return client.close();
+  }
+  return Promise.resolve();
+}
