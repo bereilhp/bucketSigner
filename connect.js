c35af202bde59c0b7477263febb61c245eb328e2
bereilhp <bereilhp@gmail.com>
2024-04-02 15:38:15 +0200

    make connection reusable

--- connect.js
+++ connect.js
@@ -1,18 +1,23 @@
 import { MongoClient } from "mongodb";
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 dotenv.config();
-const uri = process.env.IST_SHARED_MONGODB; 
+
+const uri = process.env.IST_SHARED_MONGODB;
+let db = null;
 
 const client = new MongoClient(uri);
-
 const dbName = "dotLocalStore";
 
 export async function connectToDatabase() {
+  if (db) {
+    return db;
+  }
+
   try {
     await client.connect();
     console.log("Connected successfully to MongoDB Atlas");
-    return client.db(dbName);
+    db = client.db(dbName);
+    return db;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
